2605c35592a471f4ac427f4cfc4012fa3993ff97
abhiixxhek <abhimez11@gmail.com>
2025-01-16 15:14:21 +0530

    project structure done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abhishek Rana",
+    author_email="abhimez11@gmail.com",
+    packages=find_packages()
+)
